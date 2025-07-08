ce915578e8dd1f18dbca00fa20371d7dfe0d83b5
Sudeep Jaiswal <jaiswal4sudeep@gmail.com>
2025-07-08 18:46:01 +0530

    July 8

--- android/src/main/java/com/sudeep/flutter_essential/FlutterEssentialPlugin.java
+++ android/src/main/java/com/sudeep/flutter_essential/FlutterEssentialPlugin.java
@@ -3,22 +3,12 @@ package com.sudeep.flutter_essential;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.net.ConnectivityManager;
-import android.net.Network;
-import android.net.NetworkCapabilities;
 import android.os.Build;
-import android.os.VibrationEffect;
-import android.os.Vibrator;
-import android.os.VibratorManager;
 import android.provider.Settings;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
 import com.google.android.gms.ads.identifier.AdvertisingIdClient;
-
-import java.net.NetworkInterface;
-import java.util.Collections;
-import java.util.List;
 
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.MethodCall;
@@ -40,16 +30,8 @@ public class FlutterEssentialPlugin implements FlutterPlugin, MethodCallHandler 
     @Override
     public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
         switch (call.method) {
-            case "isVpnConnected":
-                result.success(isVpnConnected());
-                break;
-
             case "getPackageInfo":
                 result.success(getPackageInfo());
-                break;
-
-            case "isInternetConnected":
-                result.success(isInternetConnected());
                 break;
 
             case "getAndroidId":
@@ -70,25 +52,6 @@ public class FlutterEssentialPlugin implements FlutterPlugin, MethodCallHandler 
             case "shareToAllApps":
                 String allContent = call.argument("content");
                 shareToAllApps(allContent, result);
-                break;
-
-            case "vibrate":
-                int duration = call.argument("duration");
-                vibrate(duration, result);
-                break;
-
-            case "vibrateWithPattern":
-                List<Integer> pattern = call.argument("pattern");
-                int repeat = call.argument("repeat");
-                vibrateWithPattern(pattern, repeat, result);
-                break;
-
-            case "cancelVibration":
-                cancelVibration(result);
-                break;
-
-            case "hasVibrator":
-                result.success(hasVibrator());
                 break;
 
             case "getInstallSource":
@@ -112,49 +75,6 @@ public class FlutterEssentialPlugin implements FlutterPlugin, MethodCallHandler 
             e.printStackTrace();
             return null;
         }
-    }
-
-    // VPN Connectivity Checker
-    private boolean isVpnConnected() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivityManager != null) {
-            for (Network network : connectivityManager.getAllNetworks()) {
-                NetworkCapabilities capabilities = connectivityManager.getNetworkCapabilities(network);
-                if (capabilities != null && capabilities.hasTransport(NetworkCapabilities.TRANSPORT_VPN)) {
-                    return true;
-                }
-            }
-        }
-
-        try {
-            List<NetworkInterface> networkInterfaces = Collections.list(NetworkInterface.getNetworkInterfaces());
-            for (NetworkInterface networkInterface : networkInterfaces) {
-                if (networkInterface.isUp() && networkInterface.getName().contains("tun")) {
-                    return true;
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        return false;
-    }
-
-    // Internet Connectivity Checker
-    private boolean isInternetConnected() {
-        ConnectivityManager connectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
-        if (connectivityManager != null) {
-            Network activeNetwork = connectivityManager.getActiveNetwork();
-            if (activeNetwork != null) {
-                NetworkCapabilities networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork);
-                if (networkCapabilities != null) {
-                    return networkCapabilities.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) &&
-                            (networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_WIFI) ||
-                                    networkCapabilities.hasTransport(NetworkCapabilities.TRANSPORT_CELLULAR));
-                }
-            }
-        }
-        return false;
     }
 
     // Package Info Getter
@@ -225,76 +145,6 @@ public class FlutterEssentialPlugin implements FlutterPlugin, MethodCallHandler 
         } catch (Exception e) {
             e.printStackTrace();
             result.error("ERROR", "Error sharing content.", e.getMessage());
-        }
-    }
-
-    private void vibrate(int duration, Result result) {
-        try {
-            Vibrator vibrator = getVibrator();
-            if (vibrator != null && vibrator.hasVibrator()) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator.vibrate(VibrationEffect.createOneShot(duration, VibrationEffect.DEFAULT_AMPLITUDE));
-                } else {
-                    vibrator.vibrate(duration);
-                }
-                result.success(true);
-            } else {
-                result.success(false);
-            }
-        } catch (Exception e) {
-            result.error("VIBRATION_ERROR", "Failed to vibrate", e.getMessage());
-        }
-    }
-
-    private void vibrateWithPattern(List<Integer> pattern, int repeat, Result result) {
-        try {
-            Vibrator vibrator = getVibrator();
-            if (vibrator != null && vibrator.hasVibrator()) {
-                long[] patternArray = new long[pattern.size()];
-                for (int i = 0; i < pattern.size(); i++) {
-                    patternArray[i] = pattern.get(i);
-                }
-
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                    vibrator.vibrate(VibrationEffect.createWaveform(patternArray, repeat));
-                } else {
-                    vibrator.vibrate(patternArray, repeat);
-                }
-                result.success(true);
-            } else {
-                result.success(false);
-            }
-        } catch (Exception e) {
-            result.error("VIBRATION_ERROR", "Failed to vibrate with pattern", e.getMessage());
-        }
-    }
-
-    private void cancelVibration(Result result) {
-        try {
-            Vibrator vibrator = getVibrator();
-            if (vibrator != null) {
-                vibrator.cancel();
-                result.success(true);
-            } else {
-                result.success(false);
-            }
-        } catch (Exception e) {
-            result.error("VIBRATION_ERROR", "Failed to cancel vibration", e.getMessage());
-        }
-    }
-
-    private boolean hasVibrator() {
-        Vibrator vibrator = getVibrator();
-        return vibrator != null && vibrator.hasVibrator();
-    }
-
-    @SuppressLint("NewApi")
-    private Vibrator getVibrator() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            VibratorManager vibratorManager = (VibratorManager) context.getSystemService(Context.VIBRATOR_MANAGER_SERVICE);
-            return vibratorManager.getDefaultVibrator();
-        } else {
-            return (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
         }
     }
 
--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,4 @@
-import 'dart:developer';
-
 import 'package:flutter/material.dart';
-import 'package:flutter_essential/flutter_essential.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,21 +12,13 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  void checkInstallSource() async {
-    final String source = await FlutterEssential.getAdvertisingId();
-    log("App installed from: $source");
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(title: const Text('Plugin example app')),
         body: Center(
-          child: ElevatedButton(
-            onPressed: () async => checkInstallSource(),
-            child: Text('Test'),
-          ),
+          child: ElevatedButton(onPressed: () {}, child: const Text('Test')),
         ),
       ),
     );
